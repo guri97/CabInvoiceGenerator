81d36afe81150193975d97c85ddd1f5dc86c0a2b
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-13 21:34:42 +0530

    [gurpreet] UC1_CalculateFareForDistanceAndTime

---
+++ CabInvoiceGenerator/TestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+namespace InvoiceGenerator
+{
+    public class CanInvoiceTest
+    {
+        InvoiceGenerator invoicegenerator;
+        [SetUp]
+        public void Setup()
+        {
+            invoicegenerator = new InvoiceGenerator(RideType.NORMAL);
+        }
+
+        [Test]
+        public void GivenDistanceAndTimeReturnTotalFair()
+        {
+            //assert
+            int distance = 2;
+            int time = 5;
+            double expected = 25;
+            InvoiceGenerator generator = new InvoiceGenerator(RideType.NORMAL);
+            //act
+
+            double actual = generator.CalculateTotalFair(distance, time);
+        }
+    }
+}
